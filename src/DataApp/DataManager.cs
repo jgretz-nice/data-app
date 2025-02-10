632029c564f02f771db8baec444969d7fae58da5
Rogerio V da Cruz <rogvc@outlook.com>
2025-02-10 16:30:54 -0700

    Refactor DataManager to use `readonly` fields and improve readability for method return codes with an enum

--- src/DataApp/DataManager.cs
+++ src/DataApp/DataManager.cs
@@ -2,8 +2,8 @@
 
 public class DataManager
 {
-    private DataFetcher _dataFetcher;
-    private DataStorage _dataStorage;
+    private readonly DataFetcher _dataFetcher;
+    private readonly DataStorage _dataStorage;
 
     public DataManager()
     {
@@ -14,22 +14,37 @@ public class DataManager
     /// <summary>
     /// Consolidate the data from the DataFetcher sources into a centralized data store.
     /// </summary>
-    /// <returns>-1 if an invalid dataId (less than 0) is provided, -2 if the data fetched from the supplied ID was null or whitespace,
-    /// or 0 if we successfully consolidated.
+    /// <returns>
+    /// <list type="bullet">
+    /// <item><description> -1 if an invalid dataId (less than 0) is provided </description></item>
+    /// <item><description> -2 if the data fetched from the supplied ID was null or whitespace </description></item>
+    /// <item><description>  0 if we successfully consolidated </description></item>
+    /// </list>
     /// </returns>
     public int ConsolidateDataFromSources(int dataId)
     {
         if (dataId < 0)
         {
-            return -1;
+            return (int)DataManagerResult.InvalidDataID;
         }
 
         var data = _dataFetcher.FetchData(dataId);
         if (string.IsNullOrWhiteSpace(data))
         {
-            return -2;
+            return (int)DataManagerResult.EmptyDataID;
         }
+
         _dataStorage.StoreData(dataId, data);
-        return 0;
+        return (int)DataManagerResult.Success;
     }
+}
+
+/// <summary>
+/// Defines possible result codes for the DataManager method calls.
+/// </summary>
+public enum DataManagerResult
+{
+    InvalidDataID = -1,
+    EmptyDataID = -2,
+    Success = 0
 }
